0c78cd94fd681ba5a431f65b154ee1cd57184ce4
Jose_Perdomo <157426260+JoesyP@users.noreply.github.com>
2024-08-22 13:42:12 -0500

    BackEnd files

---
+++ functions.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+def create_assistant(client):
+  assistant_file_path = 'assistant.json'
+
+  if os.path.exists(assistant_file_path):
+    with open(assistant_file_path, 'r') as file:
+      assistant_data = json.load(file)
+      assistant_id = assistant_data['assistant_id']
+      print("Loaded existing assistant ID.")
+  else:
+    file = client.files.create(file=open("knowledge.docx", "rb"),
+                               purpose='assistants')
+
+    assistant = client.beta.assistants.create(instructions="""
+          The assistant,CodeBuddy assistant, has been programmed to help Guide users through coding challenges by providing hints and explanations while keeping them short and consise rather than direct answers, unless explicitly requested.
+Focus on understanding the user's thought process, encouraging them to explore different approaches, and helping them learn from their mistakes.
+          """,
+                                              model="gpt-4-1106-preview",
+                                              tools=[{
+                                                  "type": "retrieval"
+                                              }],
+                                              file_ids=[file.id])
+
+    with open(assistant_file_path, 'w') as file:
+      json.dump({'assistant_id': assistant.id}, file)
+      print("Created a new assistant and saved the ID.")
+
+    assistant_id = assistant.id
+
+  return assistant_id
